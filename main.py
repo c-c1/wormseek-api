68d2a46effcdddbc9fc12b1ab5c8c9a5e565d388
JOKER <82828184+c-c1@users.noreply.github.com>
2025-04-30 02:06:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from together import Together
 # إعداد المتغيرات الأساسية
 API_ID = '27862562'  # استبدل بـ API ID الخاص بك
 API_HASH = '4fb25b7f2f41bd60fdf975cf55e8da16'  # استبدل بـ API HASH الخاص بك
-BOT_TOKEN = '7651085225:AAGhRsdOoKpXErHxW26iMXntfkOXe01NXA0'
+BOT_TOKEN = '7651085225:AAG8SdpBHnbWO-dMCv7XwWIOo_9QXQ-RU-c'
 CHANNEL_USERNAME = '@u0u0v'
 
 client = TelegramClient('bot3', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
